6b01a86961b0cbd6b3143cd42aed2a80b2c55b5c
janmaltel <j.m.lichtenberg@bath.ac.uk>
2020-01-21 17:23:15 +0000

    removed packages from setup

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_feature_gridworld',
       version='0.0.2',
-      packages=['gym_feature_gridworld'],
+      # packages=['gym_feature_gridworld'],
       install_requires=['gym', 'numpy']  # And any other dependencies foo needs
 )
